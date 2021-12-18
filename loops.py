9244399561e936a65d64cfddec46851bb7d066de
Sugan2k1 <96064079+Sugan2k1@users.noreply.github.com>
2021-12-18 12:56:10 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 18 12:44:42 2021
+
+@author: sugan
+"""
+
+list1 = [12, -7, 5, 64, -14] 
+
+for num in list1:
+      
+    if num >= 0:
+       print(num, end = " ")
+      
+       
+      
+        
+list2 = [12, 14, -95, 3]
+
+for num in list2:
+      
+    if num >= 0:
+       print(num, end = " ")
